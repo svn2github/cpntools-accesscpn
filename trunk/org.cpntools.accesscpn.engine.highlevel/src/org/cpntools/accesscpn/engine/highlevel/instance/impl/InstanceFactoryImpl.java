604b17505f04f32a7616c97bdf080a4ebc4df04f
mwesterg <mwesterg@83e89a13-d7ff-4fff-b021-40a13f624d75>
2011-02-04 10:49:37 +0000

    Add copyright notice
    git-svn-id: https://svn.win.tue.nl/repos/cpntools/AccessCPN@28 83e89a13-d7ff-4fff-b021-40a13f624d75

--- trunk/org.cpntools.accesscpn.engine.highlevel/src/org/cpntools/accesscpn/engine/highlevel/instance/impl/InstanceFactoryImpl.java
+++ trunk/org.cpntools.accesscpn.engine.highlevel/src/org/cpntools/accesscpn/engine/highlevel/instance/impl/InstanceFactoryImpl.java
@@ -1,3 +1,22 @@
+/************************************************************************/
+/* Access/CPN                                                           */
+/* Copyright 2010-2011 AIS Group, Eindhoven University of Technology    */
+/*                                                                      */
+/* This library is free software; you can redistribute it and/or        */
+/* modify it under the terms of the GNU Lesser General Public           */
+/* License as published by the Free Software Foundation; either         */
+/* version 2.1 of the License, or (at your option) any later version.   */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful,      */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    */
+/* Lesser General Public License for more details.                      */
+/*                                                                      */
+/* You should have received a copy of the GNU Lesser General Public     */
+/* License along with this library; if not, write to the Free Software  */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,           */
+/* MA  02110-1301  USA                                                  */
+/************************************************************************/
 /**
  * <copyright> </copyright> $Id$
  */
@@ -36,7 +55,7 @@ public class InstanceFactoryImpl extends EFactoryImpl implements InstanceFactory
 	 */
 	public static InstanceFactoryImpl init() {
 		try {
-			InstanceFactoryImpl theInstanceFactory = (InstanceFactoryImpl)EPackage.Registry.INSTANCE.getEFactory("http:///dk/au/daimi/ascoveco/cpn/engine/highlevel/instance.ecore"); 
+			InstanceFactoryImpl theInstanceFactory = (InstanceFactoryImpl)EPackage.Registry.INSTANCE.getEFactory("http:///org/cpntools/accesscpn/engine/highlevel/instance.ecore"); 
 			if (theInstanceFactory != null) {
 				return theInstanceFactory;
 			}
--- trunk/org.cpntools.accesscpn.engine.highlevel/src/org/cpntools/accesscpn/engine/highlevel/instance/impl/InstancePackageImpl.java
+++ trunk/org.cpntools.accesscpn.engine.highlevel/src/org/cpntools/accesscpn/engine/highlevel/instance/impl/InstancePackageImpl.java
@@ -1,3 +1,22 @@
+/************************************************************************/
+/* Access/CPN                                                           */
+/* Copyright 2010-2011 AIS Group, Eindhoven University of Technology    */
+/*                                                                      */
+/* This library is free software; you can redistribute it and/or        */
+/* modify it under the terms of the GNU Lesser General Public           */
+/* License as published by the Free Software Foundation; either         */
+/* version 2.1 of the License, or (at your option) any later version.   */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful,      */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    */
+/* Lesser General Public License for more details.                      */
+/*                                                                      */
+/* You should have received a copy of the GNU Lesser General Public     */
+/* License along with this library; if not, write to the Free Software  */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,           */
+/* MA  02110-1301  USA                                                  */
+/************************************************************************/
 /**
  * <copyright>
  * </copyright>
@@ -62,7 +81,7 @@ public class InstancePackageImpl extends EPackageImpl {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public static final String eNS_URI = "http:///dk/au/daimi/ascoveco/cpn/engine/highlevel/instance.ecore";
+	public static final String eNS_URI = "http:///org/cpntools/accesscpn/engine/highlevel/instance.ecore";
 
 	/**
 	 * The package namespace name.
@@ -70,7 +89,7 @@ public class InstancePackageImpl extends EPackageImpl {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public static final String eNS_PREFIX = "dk.au.daimi.ascoveco.cpn.engine.highlevel.instance";
+	public static final String eNS_PREFIX = "org.cpntools.accesscpn.engine.highlevel.instance";
 
 	/**
 	 * The singleton instance of the package.
