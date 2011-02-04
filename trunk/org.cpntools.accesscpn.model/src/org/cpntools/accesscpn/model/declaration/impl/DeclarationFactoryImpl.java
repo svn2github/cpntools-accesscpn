cae8c0b693f069d9e2bfb99172c1ee52a0da0382
mwesterg <mwesterg@83e89a13-d7ff-4fff-b021-40a13f624d75>
2011-02-04 11:16:03 +0000

    git-svn-id: https://svn.win.tue.nl/repos/cpntools/AccessCPN@41 83e89a13-d7ff-4fff-b021-40a13f624d75

--- trunk/org.cpntools.accesscpn.model/src/org/cpntools/accesscpn/model/declaration/impl/DeclarationFactoryImpl.java
+++ trunk/org.cpntools.accesscpn.model/src/org/cpntools/accesscpn/model/declaration/impl/DeclarationFactoryImpl.java
@@ -1,3 +1,22 @@
+/************************************************************************/
+/* Access/CPN                                                           */
+/* Copyright 2010-2011 AIS Group, Eindhoven University of Technology    */
+/*                                                                      */
+/* This library is free software; you can redistribute it and/or        */
+/* modify it under the terms of the GNU Lesser General Public           */
+/* License as published by the Free Software Foundation; either         */
+/* version 2.1 of the License, or (at your option) any later version.   */
+/*                                                                      */
+/* This library is distributed in the hope that it will be useful,      */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    */
+/* Lesser General Public License for more details.                      */
+/*                                                                      */
+/* You should have received a copy of the GNU Lesser General Public     */
+/* License along with this library; if not, write to the Free Software  */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,           */
+/* MA  02110-1301  USA                                                  */
+/************************************************************************/
 /**
  * <copyright>
  * </copyright>
@@ -37,7 +56,7 @@ public class DeclarationFactoryImpl extends EFactoryImpl implements DeclarationF
 	 */
 	public static DeclarationFactoryImpl init() {
 		try {
-			DeclarationFactoryImpl theDeclarationFactory = (DeclarationFactoryImpl)EPackage.Registry.INSTANCE.getEFactory("http:///dk/au/daimi/ascoveco/cpn/model/declaration.ecore"); 
+			DeclarationFactoryImpl theDeclarationFactory = (DeclarationFactoryImpl)EPackage.Registry.INSTANCE.getEFactory("http:///org/cpntools/accesscpn/model/declaration.ecore"); 
 			if (theDeclarationFactory != null) {
 				return theDeclarationFactory;
 			}
