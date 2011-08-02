7ba9fc2427cd3de140b6ecce3ab88976c2839410
mwesterg <mwesterg@83e89a13-d7ff-4fff-b021-40a13f624d75>
2011-08-02 12:42:14 +0000

    Switch locking strategy in ExecutionContext (from reentrant lock to standard mutex)
    git-svn-id: https://svn.win.tue.nl/repos/cpntools/AccessCPN@224 83e89a13-d7ff-4fff-b021-40a13f624d75

--- trunk/org.cpntools.accesscpn.cosimulation/src/org/cpntools/accesscpn/cosimulation/ExecutionContext.java
+++ trunk/org.cpntools.accesscpn.cosimulation/src/org/cpntools/accesscpn/cosimulation/ExecutionContext.java
@@ -1,28 +1,36 @@
 package org.cpntools.accesscpn.cosimulation;
 
 import java.util.HashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 
 /**
  * @author michael
  */
 public class ExecutionContext extends HashMap<String, Object> {
 	private static final long serialVersionUID = 1L;
-	private final Lock lock = new ReentrantLock();
+	private final Semaphore lock = new Semaphore(1);
 
 	/**
 	 * Lock execution context (and prevent simulation). Should be used in subpage-plug-ins or superpage plug-ins to lock
 	 * simulation which an atomic non-simulation action is taken.
 	 */
 	public void lock() {
-		lock.lock();
+		boolean done = false;
+		while (!done) {
+			try {
+				lock.acquire();
+				done = true;
+			} catch (final InterruptedException e) { // I would like to extend my death wishes to anybody who thought
+// returning with an InterruptedException when acquiring a lock withou notifying that you didn't obtain the lock!
+				done = false;
+			}
+		}
 	}
 
 	/**
 	 * Unlock execution context.
 	 */
 	public void unlock() {
-		lock.unlock();
+		lock.release();
 	}
 }
